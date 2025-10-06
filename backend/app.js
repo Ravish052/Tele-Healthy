7b8a23f115e00cf2cf30bafec336e4e9f1723213
Ravish052 <bhatnagar463@gmail.com>
2025-10-06 19:54:41 +0530

    added cookie handling

--- backend/app.js
+++ backend/app.js
@@ -24,4 +24,25 @@ app.all("/{*any}", (req, res, next) => {
 
 app.use(globalErrorHandler);
 
+const signToken = (userId) => {
+    return jwt.sign({
+        id : userId
+    }, process.env.JWT_SECRET, {
+        expiresIn : process.env.JWT_EXPIRES_IN || '90d'
+    })
+}
+
+const createToken = (user, statusCode, res, message) => {
+    const token = signToken(user._id);
+
+    const cookieOptions = {
+        expires: new Date(Date.now() + (process.env.JWT_COOKIE_EXPIRES_IN || 90) * 24 * 60 * 60 * 1000),
+        httponly: true,
+        secure: process.env.NODE_ENV === 'production',
+        sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax'
+    }
+}
+
+res.cookie("token", token, cookieOptions);
+
 module.exports = app;
