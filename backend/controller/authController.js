62e83257cba814aee780c847c1c5ab33f293a9a6
Ravish052 <bhatnagar463@gmail.com>
2025-10-07 23:20:47 +0530

    added verify account controller

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -26,18 +26,54 @@ exports.signup = catchAsync(async (req, res, next) => {
         otp,
         otpExpires,
     })
+
+    try {
+        await sendEmail({
+            email: newUser.email,
+            subject: "Account Verification OTP",
+            html: `<p>Your OTP for account verification is ${otp}. It is valid for 24 hours.</p>`
+        });
+
+        createSendToken(newuser, 200, res, "User registered successfully!");
+    } catch (error) {
+        console.error("Error sending email", error);
+        await user.findByIdAndDelete(newUser._id);
+        return next(new AppError("There was an error sending the email. Please try again later.", 500));
+    }
 });
 
-try {
-    await sendEmail ({
-        email : newUser.email,
-        subject : "Account Verification OTP",
-        html : `<p>Your OTP for account verification is ${otp}. It is valid for 24 hours.</p>`
-    });
+exports.verifyAccount = catchAsync(async(req, res, next) => {
+    const { email, otp} = req.body;
 
-    createSendToken(newuser, 200, res, "User registered successfully!");
-} catch (error) {
-    console.error("Error sending email", error);
-    await user.findByIdAndDelete(newUser._id);
-    return next (new AppError("There was an error sending the email. Please try again later.", 500));
-}
+    if( !email || !otp) {
+        return next ( new AppError( " Please provide email and otp", 400));
+    }
+
+    const user = await User.findOne({email});
+
+    if(!user){
+        return next(new AppError("No user found with this email", 404));
+    }
+
+    if (user.otp != otp ){
+        return next (new AppError("Invalid otp", 400));
+    }
+
+    if(Date.now() > user.otpExpires) {
+        return next(
+            new AppError("OTP has expired. Please request a new one.", 400)
+        )
+    }
+
+    user.isVerified = true;
+    user.otp = undefined;
+    user.otpExpires = undefined;
+
+    await user.save({ validateBeforeSave: false });
+
+    res.status(200).json({
+        status : "Success",
+        message: "Account verified successfully",
+    })
+})
+
